43b9428cba5f05ff29cc30c60368db88f7406c9d
dreambgnw10 <jardsocial@proton.me>
2025-12-10 06:06:24 +0900

    delete_files

--- themes/hogusource/static/js/auth-anim.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const wrappers = document.querySelectorAll(".author-wrapper");
-
-  wrappers.forEach(wrapper => {
-    wrapper.addEventListener("click", () => {
-      wrapper.classList.toggle("tapped");
-    });
-  });
-});
